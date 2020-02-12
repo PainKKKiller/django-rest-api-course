da6b4d22de5bd96fcd775175dd963357b1990664
PainKKKiller <dmitry_malugin@hotmail.com>
2020-02-12 21:52:52 +0300

    added core and user model

---
+++ cmd.sh
@@ -0,0 +1,5 @@
+sudo docker-compose run app sh -c "django-admin.py startproject app ."
+sudo docker-compose run app sh -c "python manage.py test"
+sudo docker-compose run app sh -c "python manage.py test && flake8"
+sudo docker-compose run app sh -c "python manage.py startapp core"
+sudo docker-compose run app sh -c "python manage.py makemigrations core"
