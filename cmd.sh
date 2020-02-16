5ec5284c4de28afa304b1fc795bbd09f052e30ba
PainKKKiller <dmitry_malugin@hotmail.com>
2020-02-16 20:14:19 +0300

    added users management endpoints

--- cmd.sh
+++ cmd.sh
@@ -1,5 +1,9 @@
+sudo docker-compose build
 sudo docker-compose run app sh -c "django-admin.py startproject app ."
 sudo docker-compose run app sh -c "python manage.py test"
 sudo docker-compose run app sh -c "python manage.py test && flake8"
 sudo docker-compose run app sh -c "python manage.py startapp core"
 sudo docker-compose run app sh -c "python manage.py makemigrations core"
+sudo docker-compose run app sh -c "python manage.py createsuperuser"
+sudo docker-compose run app sh -c "python manage.py startapp user"
+sudo docker-compose up
